0a8d6ef6713346d97677f4a28ed65f05ab1d1d17
Dhruv Sapra <saprad@vcu.edu>
2022-02-28 16:30:13 -0500

    Home screen added
    completed home screen, need to fix button constraints

--- app/src/main/java/com/example/a475game/MainActivity.java
+++ app/src/main/java/com/example/a475game/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.a475game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton startButton = (ImageButton) findViewById(R.id.sButton);
+        ImageButton quickPlay = (ImageButton) findViewById(R.id.imageButton2);
+        ImageButton tutorial = (ImageButton) findViewById(R.id.imageButton3);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, start_activity.class));
+            }
+        });
+
+
     }
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/a475game/start_activity.java
@@ -0,0 +1,14 @@
+package com.example.a475game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class start_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_activity);
+    }
+}
