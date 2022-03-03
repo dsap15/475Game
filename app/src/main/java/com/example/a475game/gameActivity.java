75bf2dbf4a2b59286cf3ecffbffb44ea877d8abc
Dhruv Sapra <saprad@vcu.edu>
2022-03-02 22:27:27 -0500

    Rough draft of first 2 3 screens
    Set up first 2, 3 screens and brainstorming gameplay

--- app/src/main/java/com/example/a475game/gameActivity.java
+++ app/src/main/java/com/example/a475game/gameActivity.java
@@ -13,56 +13,66 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-    public class gameActivity extends AppCompatActivity implements View.OnTouchListener {
-        ImageView imageView;
-        Bitmap bitmap;
-        Canvas canvas;
-        Paint paint;
-        float downx = 0, downy = 0, upx = 0, upy = 0;
+    public class gameActivity extends AppCompatActivity {
+//        ImageView imageView;
+//        Bitmap bitmap;
+//        Canvas canvas;
+//        Paint paint;
+//        float downx = 0, downy = 0, upx = 0, upy = 0;
+
 
 
         @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        ImageButton rect = (ImageButton) findViewById(R.id.vrect);
 
-            imageView = (ImageView) this.findViewById(R.id.imageView3);
+        rect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rect.setBackgroundColor(Color.RED);
 
-            Display currentDisplay = getWindowManager().getDefaultDisplay();
-            float dw = currentDisplay.getWidth();
-            float dh = currentDisplay.getHeight();
-
-            bitmap = Bitmap.createBitmap((int) dw, (int) dh,
-                    Bitmap.Config.ARGB_8888);
-            canvas = new Canvas(bitmap);
-            paint = new Paint();
-            paint.setColor(Color.GREEN);
-            paint.setStrokeWidth(5f);
-            imageView.setImageBitmap(bitmap);
-
-            imageView.setOnTouchListener(this);
-        }
-
-        public boolean onTouch(View v, MotionEvent event) {
-            int action = event.getAction();
-            switch (action) {
-                case MotionEvent.ACTION_DOWN:
-                    downx = event.getX();
-                    downy = event.getY();
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                    break;
-                case MotionEvent.ACTION_UP:
-                    upx = event.getX();
-                    upy = event.getY();
-                    canvas.drawLine(downx, downy, upx, upy, paint);
-                    imageView.invalidate();
-                    break;
-                case MotionEvent.ACTION_CANCEL:
-                    break;
-                default:
-                    break;
             }
-            return true;
+        });
+
+//            imageView = (ImageView) this.findViewById(R.id.imageView3);
+//
+//            Display currentDisplay = getWindowManager().getDefaultDisplay();
+//            float dw = currentDisplay.getWidth();
+//            float dh = currentDisplay.getHeight();
+//
+//            bitmap = Bitmap.createBitmap((int) dw, (int) dh,
+//                    Bitmap.Config.ARGB_8888);
+//            canvas = new Canvas(bitmap);
+//            paint = new Paint();
+//            paint.setColor(Color.GREEN);
+//            paint.setStrokeWidth(5f);
+//            imageView.setImageBitmap(bitmap);
+//
+//            imageView.setOnTouchListener(this);
         }
+
+//        public boolean onTouch(View v, MotionEvent event) {
+//            int action = event.getAction();
+//            switch (action) {
+//                case MotionEvent.ACTION_DOWN:
+//                    downx = event.getX();
+//                    downy = event.getY();
+//                    break;
+//                case MotionEvent.ACTION_MOVE:
+//                    break;
+//                case MotionEvent.ACTION_UP:
+//                    upx = event.getX();
+//                    upy = event.getY();
+//                    canvas.drawLine(downx, downy, upx, upy, paint);
+//                    imageView.invalidate();
+//                    break;
+//                case MotionEvent.ACTION_CANCEL:
+//                    break;
+//                default:
+//                    break;
+//            }
+//            return true;
+//        }
     }
--- app/src/main/java/com/example/a475game/tutorialActivity.java
+++ app/src/main/java/com/example/a475game/tutorialActivity.java
@@ -2,7 +2,10 @@ package com.example.a475game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class tutorialActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class tutorialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+
+        ImageButton tutorialBtn = (ImageButton) findViewById(R.id.tutorialBack);
+        tutorialBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tutorialActivity.this, MainActivity.class));
+            }
+        });
+
     }
 }
