90e08ba116ba6f7a647eebbe319a92c46db97fd0
Dhruv Sapra <saprad@vcu.edu>
2022-03-25 00:59:04 -0400

    added partial gameplay
    partial gameplay functionality

--- app/src/main/java/com/example/a475game/gameActivity.java
+++ app/src/main/java/com/example/a475game/gameActivity.java
@@ -1,78 +1,155 @@
-    package com.example.a475game;
+package com.example.a475game;
+import static java.lang.Math.min;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-    public class gameActivity extends AppCompatActivity {
-//        ImageView imageView;
-//        Bitmap bitmap;
-//        Canvas canvas;
-//        Paint paint;
-//        float downx = 0, downy = 0, upx = 0, upy = 0;
+import java.util.ArrayList;
 
 
+public class gameActivity extends AppCompatActivity {
+    private Canvas canvas;
+    private Bitmap bitmap;
+    private ImageView imageView;
+    Dot[][] dots;
+    private boolean player1Turn = true;
+    private final int player1Color = Color.rgb(0, 0, 255);
+    private final int player2Color = Color.rgb(255, 0, 0);
 
+
+
+    @SuppressLint("ClickableViewAccessibility")
         @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+        {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        ImageButton rect = (ImageButton) findViewById(R.id.vrect);
+            imageView = (ImageView) findViewById(R.id.myimageview);
+            LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linear_layout);
+            linearLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener()
+            {
+                @Override
+                public void onGlobalLayout() {
+                    linearLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    bitmap = Bitmap.createBitmap(imageView.getWidth(), imageView.getHeight(), Bitmap.Config.ARGB_8888);
+                    canvas = new Canvas(bitmap);
+                    imageView.setImageBitmap(bitmap);
+                    dots = generateDotGrid(3, imageView.getWidth(), imageView.getHeight());
+                    drawDots(dots, canvas);
+                }
+            });
+            imageView.setOnTouchListener(new View.OnTouchListener()
+            {
+                public boolean onTouch(View v, MotionEvent event)
+                {
+                    if (event.getAction() == MotionEvent.ACTION_DOWN)
+                        onClick(event.getX(), event.getY());
+                    return true;
+                }
+            });
 
-        rect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                rect.setBackgroundColor(Color.RED);
+        }
+    private Dot[][] generateDotGrid(int numDots, int screenWidth, int screenHeight) {
+        float borderProportion = 0.2f;
 
+        float boxSpaceWidth = (1 - 2 * borderProportion) * screenWidth;
+        float boxSpaceHeight = (1 - 2 * borderProportion) * screenHeight;
+
+        float boxSpaceSideLength = min(boxSpaceWidth, boxSpaceHeight);
+
+        float cellSideLength = boxSpaceSideLength / (numDots - 1);
+
+        float centerX = screenWidth / 2.0f;
+        float centerY = screenHeight / 2.0f;
+
+        float startX = centerX - boxSpaceSideLength / 2.0f;
+        float startY = centerY - boxSpaceSideLength / 2.0f;
+
+        Dot[][] dots = new Dot[numDots][numDots];
+        for (int r = 0; r < numDots; r++) {
+            for (int c = 0; c < numDots; c++) {
+                dots[r][c] = new Dot((startX + c * cellSideLength), (startY + r * cellSideLength), 25f);
             }
-        });
+        }
+        return dots;
+    }
+    private void drawDots(Dot[][] dots, Canvas canvas){
+        Paint paint = new Paint();
+        paint.setARGB(255,0,0,0);
+        for(int r = 0; r < dots.length; r++){
+            for(int c =0; c < dots[r].length; c++){
+                Dot dotsForDrawing = dots[r][c];
+                canvas.drawCircle(dotsForDrawing.x, dotsForDrawing.y,dotsForDrawing.radius, paint);
+            }
+        }
+    }
 
-//            imageView = (ImageView) this.findViewById(R.id.imageView3);
-//
-//            Display currentDisplay = getWindowManager().getDefaultDisplay();
-//            float dw = currentDisplay.getWidth();
-//            float dh = currentDisplay.getHeight();
-//
-//            bitmap = Bitmap.createBitmap((int) dw, (int) dh,
-//                    Bitmap.Config.ARGB_8888);
-//            canvas = new Canvas(bitmap);
-//            paint = new Paint();
-//            paint.setColor(Color.GREEN);
-//            paint.setStrokeWidth(5f);
-//            imageView.setImageBitmap(bitmap);
-//
-//            imageView.setOnTouchListener(this);
+    private Index2D getClickedDotIndex(float cx, float cy, Dot[][] dots){
+        Index2D index2D = null;
+        for (int i = 0; i < dots.length; i++){
+            for( int j = 0; j < dots[i].length; j++){
+                if (dots[i][j].contains(cx, cy)){
+                    index2D = new Index2D(i,j);
+                }
+            }
+        }
+        return index2D;
+    }
+
+    private Index2D firstDotClickedIndex = null;
+    private void onClick(float clickX, float clickY) {
+
+        Index2D dotIndex = getClickedDotIndex(clickX, clickY, dots);
+        if(dotIndex == null) // if the user didn't click a dot
+            return;
+
+        if(firstDotClickedIndex == null){
+            firstDotClickedIndex = dotIndex;
+            return;
         }
 
-//        public boolean onTouch(View v, MotionEvent event) {
-//            int action = event.getAction();
-//            switch (action) {
-//                case MotionEvent.ACTION_DOWN:
-//                    downx = event.getX();
-//                    downy = event.getY();
-//                    break;
-//                case MotionEvent.ACTION_MOVE:
-//                    break;
-//                case MotionEvent.ACTION_UP:
-//                    upx = event.getX();
-//                    upy = event.getY();
-//                    canvas.drawLine(downx, downy, upx, upy, paint);
-//                    imageView.invalidate();
-//                    break;
-//                case MotionEvent.ACTION_CANCEL:
-//                    break;
-//                default:
-//                    break;
-//            }
-//            return true;
-//        }
+        if(!dotIndex.isAdjacentTo(firstDotClickedIndex)){
+            firstDotClickedIndex = dotIndex;
+            return;
+        }
+
+        Dot firstDotClicked = dots[firstDotClickedIndex.row][firstDotClickedIndex.col];
+        Dot clickedDot = dots[dotIndex.row][dotIndex.col];
+        Paint linePaint = new Paint();
+        if(player1Turn)
+            linePaint.setColor(player1Color);
+        else
+            linePaint.setColor(player2Color);
+
+        linePaint.setStrokeWidth(clickedDot.radius / 3);
+        canvas.drawLine(clickedDot.x, clickedDot.y, firstDotClicked.x, firstDotClicked.y,linePaint);
+        firstDotClickedIndex = null;
+
+        player1Turn = !player1Turn;
+
+        imageView.invalidate();
+        }
+        /// we need to:
+        // fill rectangle
+        // keep and update score
+        // if last edge makes a box update score and dont switch the player
+        
+
+
     }
